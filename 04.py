215b624070e1c0367e63cca2a61aaf98eac12b94
Marc Scott <marc@raspberrypi.org>
2019-12-04 15:27:26 +0000

    04 complete

--- 04.py
+++ 04.py
@@ -1,6 +1,8 @@
+import re
+
 possible_passwords = [str(i) for i in range(108457,562041+1)]
 
-def detect_adjacent_1(string):
+def detect_adjacent(string):
     for i in range(len(string) - 1):
         if string[i] == string[i+1]:
             return True
@@ -12,15 +14,14 @@ def detect_increasing(string):
         if int(string[i]) > int(string[i+1]):
             increasing = False
     return increasing
-        
-def detect_adjacent_2(string):
-    for i in range(len(string)):
-        group = string[i:i+3]
-        group = [i for i in group]
-        if len(group) == 3:
-            same = all(x == group[0] for x in group)
-            
-        if same
-            
-        
-matches = [i for i in possible_passwords if detect_adjacent(i) and detect_increasing(i)]
+
+def doubles(string):
+    number = set(re.findall(r'([0-9])\1', string))
+    repeats = [re.findall(r"({0}{0}+)".format(c), string) for num in numbers]
+    if [x for x in repeats if len(x[0]) == 2]:
+        return True
+    else:
+        return False        
+
+matches_1 = [i for i in possible_passwords if detect_adjacent(i) and detect_increasing(i)]
+matches_2 = [i for i in possible_passwords if doubles(i) and detect_increasing(i)]
